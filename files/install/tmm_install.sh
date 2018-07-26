9f3dfa246d6918632fc76bdf5d2568005d461f0e
kuhsaber <41760473+kuhsaber@users.noreply.github.com>
2018-07-26 23:36:45 +0200

    Update tmm_install.sh

--- files/install/tmm_install.sh
+++ files/install/tmm_install.sh
@@ -29,8 +29,8 @@ cat <<'EOT' > /etc/my_init.d/04_tmm_config.sh
 [[ ! -L /tinyMediaManager/config.xml ]] && ln -s /config/config.xml /tinyMediaManager/config.xml
 [[ ! -L /tinyMediaManager/tmm.odb ]] && ln -s /config/tmm.odb /tinyMediaManager/tmm.odb
 
+chown -R root: /mnt
 chown -R nobody:users /config /tinyMediaManager /nobody || exit 0
-chown -R root: /mnt || exit 0
 EOT
 
 chmod -R +x /etc/service/ /etc/my_init.d/
